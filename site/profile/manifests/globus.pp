b0f370e6b40d9c42f05b815f6e5bae87dca09a77
root <root@mgmt1.int.prune.dev-sd4h.ca>
2024-02-15 21:55:04 +0000

    Add dependencies for poquirion/puppet-module-globus

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -1,17 +1,34 @@
-class profile::globus {
+class profile::globus(
+  String $display_name,
+  String $organization,
+  String $owner,
+  String $contact_email,
+  Array[String] $accepted_domains
+
+) {
   package { 'wget':
     ensure => installed,
   }
 
-  $public_ip = lookup("terraform.instances.${facts['networking']['hostname']}.public_ip")
-  class { 'globus':
-    display_name  => $globus::display_name,
-    client_id     => $globus::client_id,
-    client_secret => $globus::client_secret,
-    contact_email => $globus::contact_email,
-    ip_address    => $public_ip,
-    organization  => $globus::organization,
-    owner         => $globus::owner,
-    require       => Package['wget'],
-  }
+$public_ip = lookup("terraform.instances.${facts['networking']['hostname']}.public_ip")
+$users = lookup("profile::users::ldap::users")
+
+class { 'globus':
+  display_name   => $display_name,
+  organization   => $organization,
+  owner          => $owner,
+  contact_email  => $contact_email,
+  ip_address    => $public_ip,
+  accepted_domains => $accepted_domains,
+  users => $users
+}
+
+#  class { 'globus':
+#    display_name  => $globus::display_name,
+#    contact_email => $globus::contact_email,
+#    ip_address    => $public_ip,
+#    organization  => $globus::organization,
+#    owner         => $globus::owner,
+#    require       => Package['wget'],
+#  }
 }
