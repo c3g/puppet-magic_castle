b388a13f89dc6a3c1c63c89d01f2ed7c45709e99
P-O Quirion <pioliqui@gmail.com>
2026-01-12 16:00:03 -0500

    cvmfs config for ref et soft mugqic

--- site/profile/manifests/cvmfs.pp
+++ site/profile/manifests/cvmfs.pp
@@ -45,6 +45,24 @@ class profile::cvmfs::client (
     require => Package['cvmfs']
   }
 
+  file { '/etc/cvmfs/config.d/ref.mugqic.local':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/profile/cvmfs/ref.mugqic.local',
+    mode   => '0644'
+  }
+
+  file { '/etc/cvmfs/config.d/soft.mugqic.local':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/profile/cvmfs/soft.mugqic.local',
+    mode   => '0644'
+  }
+
+
+
   file { '/etc/cvmfs/default.local.ctmpl':
     content => epp('profile/cvmfs/default.local', {
         'strict_mount' => $strict_mount ? { true => 'yes', false => 'no' }, # lint:ignore:selector_inside_resource
